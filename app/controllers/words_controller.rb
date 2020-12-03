7ea3c0fafb9c6131a29fbf18cf973e5a777a4490
Alexey Zotov <zotov.alexei.1@yandex.ru>
2020-12-03 13:42:55 +0300

    index page, add bootstrap

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :edit, :update, :destroy]
+  before_action :set_word, only: %i[show edit update destroy]
 
   # GET /words
   # GET /words.json
@@ -62,13 +62,11 @@ class WordsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_word
-      @word = Word.find(params[:id])
-    end
+  def set_word
+    @word = Word.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def word_params
-      params.require(:word).permit(:word, :translation, :synonyms, :example)
-    end
+  def word_params
+    params.require(:word).permit(:word, :translation, :synonyms, :example)
+  end
 end
