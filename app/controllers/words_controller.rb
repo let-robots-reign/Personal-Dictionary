675313246be923ef9933a195674741ec741a0dd1
Alexey Zotov <zotov.alexei.1@yandex.ru>
2020-12-09 21:08:03 +0300

    authentication with devise

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,6 @@
 class WordsController < ApplicationController
   before_action :set_word, only: %i[show edit update destroy]
-
+  before_action :authenticate_user!, only: %i[new show create update destroy edit]
   # GET /words
   # GET /words.json
   def index
