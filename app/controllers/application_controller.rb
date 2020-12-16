6935cf579ef9cc9972e4e64bc103946fa44d01e4
Alexey Zotov <zotov.alexei.1@yandex.ru>
2020-12-16 21:47:52 +0300

    localize devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action do
+    I18n.locale = :ru # Or whatever logic you use to choose.
+  end
 end
